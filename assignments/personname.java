944c943ae75906aabc8ab77a8e76f34419343f28
Rahul kucharla <rahulkucharla20@gmail.com>
2021-05-22 20:09:14 +0530

    Added Week3 Programs

---
+++ assignments/personname.java
@@ -0,0 +1,18 @@
+ class personname {
+     public static void main(String args[])
+     {
+         String first,last,middle;
+         first=args[0];
+         middle=args[1];
+         last=args[2];
+         System.out.println("Fullname: "+first+" "+middle+" "+last+" ");
+         System.out.println("First name and last name:"+first+" "+last+" ");
+         System.out.println("First name and middle name:"+first+" "+middle+" ");
+         System.out.println("Middle name and first name:"+middle+" "+first+" ");
+         System.out.println("Middle name and last name:"+middle+" "+last+" ");
+         System.out.println("Last name and first name:"+last+" "+first+" ");
+         System.out.println("Last name and middle name:"+last+" "+middle+" ");
+
+     }
+    
+}
---
+++ week3/Week3C.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class Week3C {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = 0;
+        System.out.println("Enter size of matrix:");
+        n = sc.nextInt();
+        int arr[][] = new int[n][n];
+        int x = 0, y = 0;
+        System.out.println("Enter elements into the matrix:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j) {
+                    x = x + arr[i][j];
+                    y = y + arr[i][n - 1 - i];
+                }
+            }
+        }
+        System.out.println("Absolute difference of fiagonal elements is:" + Math.abs(x - y));
+    }
+}
